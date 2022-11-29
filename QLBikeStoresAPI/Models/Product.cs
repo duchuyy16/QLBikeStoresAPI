10f5e2e22f32480484868544e24c0b4ebd364abb
Bui DucHuy <duchuyy16@gmail.com>
2022-11-29 16:37:32 +0700

    Add project files.

---
+++ QLBikeStoresAPI/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace QLBikeStoresAPI.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            OrderItems = new HashSet<OrderItem>();
+            Stocks = new HashSet<Stock>();
+        }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int BrandId { get; set; }
+        public int CategoryId { get; set; }
+        public short ModelYear { get; set; }
+        public decimal ListPrice { get; set; }
+        public string ImageBike { get; set; }
+        public decimal Discount { get; set; }
+
+        public virtual Brand Brand { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+        public virtual ICollection<Stock> Stocks { get; set; }
+    }
+}
